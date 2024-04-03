58a75c225f5c9eeccdc450471b1644bf26c28b88
Carlo Gimps <41587193+cleverguns@users.noreply.github.com>
2024-04-03 11:49:40 +0800

    new changes
    updated 4/3/24

---
+++ Ens-SaaS Dashboard/src/features/user/components/TemplatePointers.js
@@ -0,0 +1,11 @@
+function TemplatePointers(){
+    return(
+        <>
+         <h1 className="text-2xl mt-8 font-bold">Welcome to Enshored SaaS Dashboard Monitoring</h1>
+         
+         
+        </>
+    )
+}
+
+export default TemplatePointers
